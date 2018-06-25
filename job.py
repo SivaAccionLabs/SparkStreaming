a7c1c655401d14881b3291faaaab3fa9160b69a5
dharaneeshvrd <dharanish185@gmail.com>
2018-06-25 14:52:56 +0000

    second heighest sallary in each department

--- job.py
+++ job.py
@@ -22,17 +22,35 @@ def write_hbase(rdd):
         print line.name
         table.put('id'+str(line.id), {'cf:name': line.name}) 
 
+def logic(rdd):
+    dg = {}
+    for line in rdd.collect():
+        line = json.loads(line)
+        print line
+        print type(line)
+        if line['dept_id'] in dg.keys():
+            dg[line['dept_id']].append(line['sallary'])
+        else:
+            dg[line['dept_id']] = []
+            dg[line['dept_id']].append(line['sallary'])
+
+    for dept in dg.keys():
+        if len(dg[dept]) >=2:
+            print str(dept) + " ===>>>" + str(sorted(dg[dept])[1])
+        else:
+            print str(dept) + " ===>>>" + str(sorted(dg[dept])[0])
 if __name__ == '__main__':
     print "Creating spark context"
-    kafka_params = {"metadata.broker.list": "10.0.0.70:9092"}
+    kafka_params = {"metadata.broker.list": "10.0.0.92:9092"}
     sc = SparkContext(appName="siva") 
-    ssc = StreamingContext(sc, 10)
-    message = KafkaUtils.createDirectStream(ssc, ["hbase"], kafka_params)
+    ssc = StreamingContext(sc, 5)
+    message = KafkaUtils.createDirectStream(ssc, ["siva10"], kafka_params)
     actual = message.map(lambda x: x[1])
-    values = actual.map(lambda x: (json.loads(x)['id'], json.loads(x)['name']))
-    values = values.map(lambda data: Row(id=data[0], name=data[1]))
+    actual.foreachRDD(logic)
+    #values = actual.map(lambda x: (json.loads(x)['id'], json.loads(x)['name']))
+    #values = values.map(lambda data: Row(id=data[0], name=data[1]))
     #values.pprint()
-    values.foreachRDD(write_hbase)  
+    #values.foreachRDD(write_hbase)  
 
     #actual.pprint()
     #counts = actual.map(lambda x: (json.loads(x)['id'], json.loads(x)['name']))
--- pro.py
+++ pro.py
@@ -2,10 +2,21 @@ from kafka import KafkaProducer
 import time
 import json
 
-topic = "hbase"
-broker_list = "10.0.0.70:9092"
+topic = "siva10"
+broker_list = "10.0.0.92:9092"
 producer = KafkaProducer(bootstrap_servers = [broker_list])
-while True:  
-    data = {"id": 5, "name": "siva"}
-    producer.send(topic, json.dumps(data))
-    time.sleep(5) 
+while True: 
+    data1 = {"id": 1, "name": "siva1", "sallary": 100, "dept_id": 1}
+    data2 = {"id": 2, "name": "siva2", "sallary": 100, "dept_id": 2}
+    data3 = {"id": 3, "name": "siva3", "sallary": 700, "dept_id": 1}
+    data4 = {"id": 4, "name": "siva4", "sallary": 1000, "dept_id": 2}
+    data5 = {"id": 5, "name": "siva5", "sallary": 200, "dept_id": 3}
+    data6 = {"id": 6, "name": "siva6", "sallary": 500, "dept_id": 3}
+    for i in range(3): 
+        producer.send(topic, json.dumps(data1))
+        producer.send(topic, json.dumps(data2))
+        producer.send(topic, json.dumps(data3))
+        producer.send(topic, json.dumps(data4))
+        producer.send(topic, json.dumps(data5))
+        producer.send(topic, json.dumps(data6))
+        time.sleep(5) 
